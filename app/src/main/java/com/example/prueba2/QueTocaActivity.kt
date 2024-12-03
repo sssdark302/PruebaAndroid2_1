0f06daf5ff62057cf426ca70c7fe06e829ce51fe
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-12-03 15:22:02 +0100

    feat/layouts

--- app/src/main/java/com/example/prueba2/QueTocaActivity.kt
+++ app/src/main/java/com/example/prueba2/QueTocaActivity.kt
@@ -1,5 +1,9 @@
 package com.example.prueba2
 
+import android.widget.TextView
+
+val tvClaseActual = findViewById<TextView>(R.id.tvClaseActual)
 FirebaseHelper().obtenerClaseActual { clase ->
-    tvClaseActual.text = clase?.let { "Estás en clase de ${it.asignatura}" } ?: "No hay clases ahora mismo"
+    tvClaseActual.text = clase?.let
+    { "Estás en clase de ${it.asignatura}" } ?: "No hay clases ahora mismo"
 }
