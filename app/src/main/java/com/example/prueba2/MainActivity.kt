87f5fea98bb14696d2ad762f4339493668481b8e
sssdark302 <158068173+sssdark302@users.noreply.github.com>
2024-12-03 15:01:52 +0100

    feat/mainactivity

---
+++ app/src/main/java/com/example/prueba2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.prueba2
+
+btnAgregarClase.setOnClickListener {
+    startActivity(Intent(this, AddClaseActivity::class.java))
+}
+
+btnVerHorario.setOnClickListener {
+    startActivity(Intent(this, VerHorarioActivity::class.java))
+}
+
+btnQueToca.setOnClickListener {
+    startActivity(Intent(this, QueTocaActivity::class.java))
+}
